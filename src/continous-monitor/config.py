b3ded3689d075a1c986857909a051268aa1b6098
Ogenbertrand <ogenbertrand@gmail.com>
2024-09-05 16:30:50 +0100

    modify the docker-compose file to fix test error

--- src/continous-monitor/config.py
+++ src/continous-monitor/config.py
@@ -4,8 +4,8 @@ import logging
 class Config:
     DB_USER = os.getenv('DB_USER', 'location-user')
     DB_PASSWORD = os.getenv('DB_PASSWORD', 'password')
-    DB_HOST = os.getenv('DB_HOST', 'localhost')
-    DB_PORT = os.getenv('DB_PORT', '5433')
+    DB_HOST = os.getenv('DB_HOST', '127.0.0.1')
+    DB_PORT = os.getenv('DB_PORT', '5432')
     DB_NAME = os.getenv('DB_NAME', 'location-db')
     
     SQLALCHEMY_DATABASE_URI = f'postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
